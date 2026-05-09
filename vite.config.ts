90f85aaadae157a5da48989fb9e8cc0ba97ebc83
sravanren11 <sravanrenjithlap3@gmail.com>
2026-05-09 22:30:35 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
-    base: "/weddingphotography-/"
+    base: "/weddingphotography/"
   }
 });
